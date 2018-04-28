f5bc5b935adcb54dfbbbfdddc1d0bc0e1615c3be
maxian <Ma@danglaoshi.net>
2018-04-28 17:53:47 +0800

    first commit

---
+++ src/main/java/cn/xian/app/Starter.java
@@ -0,0 +1,15 @@
+package cn.xian.app;
+
+import io.vertx.core.Launcher;
+import top.onceio.core.annotation.BeansIn;
+import top.onceio.plugins.vertx.OIOVerticle;
+
+@BeansIn("cn.xian.app")
+public class Starter extends OIOVerticle {
+
+	public static void main(String[] args) {
+		
+    	Launcher.main(new String[]{"run",Starter.class.getName(),"-conf","src/main/java/config.json"});
+	}
+
+}
---
+++ src/main/java/cn/xian/app/api/UserApi.java
@@ -0,0 +1,29 @@
+package cn.xian.app.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.Json;
+import top.onceio.core.mvc.annocations.Api;
+import top.onceio.core.mvc.annocations.Param;
+
+@Api("/user")
+public class UserApi {
+	
+	@Api("/signup/{username}")
+	public Map<String,Object> signup(@Param("username") String username, @Param("passwd") String passwd) {
+		Map<String,Object> map = new HashMap<>();
+		map.put("username", username);
+		map.put("passwd", passwd);
+		return map;
+	}
+	
+	@Api("/config/")
+	public void conf(@Param("arg") String arg,HttpServerRequest req) {
+		Map<String,Object> map = new HashMap<>();
+		map.put("arg", arg);
+		map.put("val", req.getHeader(arg));
+		req.response().end(Json.encode(map));
+	}
+}
