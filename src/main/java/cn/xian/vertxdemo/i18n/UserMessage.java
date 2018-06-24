f2462edaa6329d08e5521f6e565215b8b60c3037
Liar <1520070240@qq.com>
2018-06-24 13:04:21 +0800

    国际化信息注解错误修正

--- src/main/java/cn/xian/vertxdemo/i18n/UserMessage.java
+++ src/main/java/cn/xian/vertxdemo/i18n/UserMessage.java
@@ -1,8 +1,8 @@
 package cn.xian.vertxdemo.i18n;
 
-import top.onceio.core.annotation.I18nCfg;
+import top.onceio.core.annotation.I18nMsg;
 
-@I18nCfg("zh")
+@I18nMsg("zh")
 public class UserMessage {
 	public static final String USER_EXIST = "账户：%s 已经被注册";
 }
